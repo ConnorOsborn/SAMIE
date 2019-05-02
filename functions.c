73d3a9255f20bd8e8d824c783958490d4d338b88
Maddy <37761862+Maddy54652@users.noreply.github.com>
2019-05-02 10:32:44 -0700

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,7 +1,19 @@
 /*
   somefunctions. I'm not finished.
-  void for the moment
+  Some code has been copied and pasted from lab 4
 */
+
+#include <Hercules.h>
+#include <Hercules_dfs.h>
+#include <Hercules_Stepper.h>
+#include <seeed_pwm.h>
+#include <Ultrasonic.h>
+
+#define PIN_LIGHT_SENSOR A0
+#define PIN_SONAR_SENSOR 11
+#define SPEED 20
+
+
 void drive(void);
 void rotate45(void);
 void rotate90(void);
@@ -10,11 +22,31 @@ void ForR(void);
 void getDirection(void);
 
 void drive(void){
-  //it drives
+  Serial.begin(9600);
+	MOTOR.begin();
+
+	MOTOR.setSpeedDir1((SPEED - 1), DIRF);
+	MOTOR.setSpeedDir2((SPEED), DIRR);
   return;
 }
 void rotate45(void){
  //it rotates 45 degrees
+  for (int speed = 0; speed < 30; speed++)
+	{
+		MOTOR.setSpeedDir1(speed, DIRF);
+		MOTOR.setSpeedDir2(speed, DIRR);
+		delay(10);
+	}
+
+	delay(160);
+
+	for (int speed = 30; speed > 0; --speed)
+	{
+		MOTOR.setSpeedDir1(speed, DIRF);
+		MOTOR.setSpeedDir2(speed, DIRR);
+		delay(10);
+	}
+	MOTOR.setSpeedDir(0, DIRF);
 }
 void rotate90(void){
  //it rotates 90 degrees 
